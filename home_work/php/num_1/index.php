df96ae5e44060e963a8a8993318561a76cd25ddc
almong <admin@ag-net.ru>
2018-08-13 22:32:05 +0300

    php-lesson-1

---
+++ home_work/php/num_1/index.php
@@ -0,0 +1,41 @@
+<?php
+$_1 = 1;
+$_2 = $_1;
+
+if (!empty($_POST)) {
+    $in = $_POST['input'];
+    if (is_numeric($in)) {
+        echo " Вы ввели: $in <br />";
+        while ($in > $_1 || $in == $_1) {
+            if ($_1 == $in){
+                echo 'Задуманное число входит в числовой ряд Фибоначчи!'; 
+                break; 
+            }else {
+            $_3 = $_1;
+            $_1 += $_2;
+            $_2 = $_3; 
+            }
+            if ($_1 > $in) {
+                echo 'Задуманное число НЕ входит в числовой ряд Фибоначчи!';
+            }
+        }
+    } else {
+    echo 'Вы ввели не число !!!';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Дополнительное задание "Введение в PHP"</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+    <p>Введите число</p>
+    <input type="text" name="input">
+    <input type="submit" value="Ввод">
+    </form>
+</body>
+</html>
