ee0d9659ce98afd97fd20701f3ac2b5871a2beef
almong <admin@ag-net.ru>
2018-08-20 19:16:23 +0300

    visa

--- home_work/php/num_3/money.php
+++ home_work/php/num_3/money.php
@@ -1,31 +1,36 @@
 <?php
-//Учёт расходов PHP Нетолония (https://github.com/netology-code/php-2-homeworks/tree/master/files/money)
-$date = date('Y-m-d');
-$fc = file('c:\\github\\netology\\home_work\\php\num_3\\money.csv');
-$file = 'c:\\github\\netology\\home_work\\php\num_3\\money.csv';
-$input = $argv;
-$input = array_splice($input, 1);
-$input = implode(" ", $input);
-//Считаем затраты за день
-if ($argv[1] === "--today"){
-    if (file_exists($file)){
-        for ($i=0; $i<count($fc); $i++){
-            $arr = explode(",", $fc[$i]);
-            if ($date === $arr[0]){
-                $sum += $arr[1];
+    //Учёт расходов PHP Нетолония (https://github.com/netology-code/php-2-homeworks/tree/master/files/money)
+    $date = date('Y-m-d');
+    $sum = 0;
+    $file = 'money.csv';
+    $input = $argv;
+    $input = array_splice($input, 1);
+    $input = implode(" ", $input);
+    //Считаем затраты за день
+    if ($argv[1] === "--today"){
+        if (file_exists($file)){
+            $fc = file('money.csv');
+            for ($i=0; $i<count($fc); $i++){
+                $arr = explode(",", $fc[$i]);
+                if ($date === $arr[0]){
+                    $sum += $arr[1];
+                }
             }
+            echo "$date расход за день: $sum";
+        } else {
+            echo 'Файл не существует';
         }
-        echo "$date расход за день: $sum";
+    //Проверяем ввод по шаблону
+    } elseif (preg_match('/^(\d+|(\d+\.\d{1,2})) \w/iu', $input)) {
+    $description = array_splice($argv, 2);
+    $description = implode(" ", $description);
+    $money_str = "$date,$argv[1],$description\r\n";
+    if (is_writable($file)){
+        file_put_contents($file, $money_str, FILE_APPEND);
     } else {
-        echo 'Файл не существует';
+        echo 'Ошибка открытия файла для записи';
     }
-//Проверяем ввод по шаблону
-} elseif (preg_match('/^\d+ \w/iu', $input)) {
-$description = array_splice($argv, 2);
-$description = implode(" ", $description);
-$money_str = "$date,$argv[1],$description\r\n";
-file_put_contents($file, $money_str, FILE_APPEND);
-} else {
-    echo 'Ошибка! Аргументы не заданы. Укажите флаг --today или запустите скрипт с аргументами {цена} и {описание покупки}';
-}
+    } else {
+        echo 'Ошибка! Аргументы не заданы. Укажите флаг --today или запустите скрипт с аргументами {цена} и {описание покупки}';
+    }
 ?>
---
+++ home_work/php/num_3/visa.php
@@ -0,0 +1,32 @@
+<?php
+    $err = 0;
+    $file = 'visa.csv';
+    if (is_writable($file)){
+        $fc = file('visa.csv');
+        for ($i=0; $i<count($fc); $i++){
+            $arr = explode(",", $fc[$i]);
+            $country = str_replace("\"", "",$arr[1]);
+            if ($argv[1] === $country){
+                $visa = str_replace("\"", "",$arr[4]);
+                $err = 0;
+                echo "$country: $visa";
+                break;
+            } else {}
+            $lev = levenshtein($argv[1],$country);
+            if ($lev <= 1){
+                $visa = str_replace("\"", "",$arr[4]);
+                $err = 0;
+                echo "$country: $visa";
+                break;
+            } else {
+                $err = 1;
+            }
+        }
+        if ($err == 1){
+            echo 'Ошибка ввода страны или страна не найдена';
+        }
+    } else {
+        echo 'Ошибка чтения файла';
+    }
+
+?>
