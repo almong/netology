79ea60a39b0f877338c3e5bb75c1e5fae2cd0ba0
almong <admin@ag-net.ru>
2018-08-26 19:05:53 +0300

    1

--- home_work/php/num_3/books.php
+++ home_work/php/num_3/books.php
@@ -5,23 +5,24 @@
         $code_str = urlencode($query);
         $json_str = file_get_contents("https://www.googleapis.com/books/v1/volumes?q=$code_str");
         $obj = json_decode($json_str);
+        
         if (json_last_error() == 'JSON_ERROR_NONE') {
-        //print_r($obj);
-        $file = __DIR__ . '/books.csv';
-        $id = $obj->items[0]->id;
-        $title = $obj->items[0]->volumeInfo->title;
-        if (!isset($obj->items[0]->volumeInfo->authors[0])){
-            $authors = 'none';
-        } else {
-            $authors = $obj->items[0]->volumeInfo->authors[0];
-        }
-        $csv_str = "$id,$title,$authors\r\n";
-        //echo $csv_str;
-        if (is_writable($file)){
-            file_put_contents($file, $csv_str, FILE_APPEND);
-        } else {
-            echo 'Ошибка открытия файла для записи';
-        }
+            $file = __DIR__ . '/books.csv';
+            $id = $obj->items[0]->id;
+            $title = $obj->items[0]->volumeInfo->title;
+            
+            if (!isset($obj->items[0]->volumeInfo->authors[0])){
+                $authors = 'none';
+            } else {
+                $authors = $obj->items[0]->volumeInfo->authors[0];
+            }
+            
+            $csv_str = "$id,$title,$authors\r\n";
+            if (is_writable($file)){
+                file_put_contents($file, $csv_str, FILE_APPEND);
+            } else {
+                echo 'Ошибка открытия файла для записи';
+            }
         } else {
             echo "Ошибка, обработки JSON\r\n";
             // throw new UnexpectedValueException(json_last_error_msg());
