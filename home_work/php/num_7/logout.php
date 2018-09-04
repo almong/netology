c78c9a3917476dc9e3e53a9382d8faa13bff2f91
almong <admin@ag-net.ru>
2018-09-04 10:24:40 +0300

    Правка logout

--- home_work/php/num_7/logout.php
+++ home_work/php/num_7/logout.php
@@ -1,6 +1,14 @@
 <?php
     session_set_cookie_params(3600);
     session_start();
-    session_destroy();
-    header('Location: /');
+    if (!empty($_SESSION['user_login']) || !empty($_SESSION['guest'])){
+        session_destroy();
+        header('Location: /');
+        exit;
+    } else {
+
+    }
+    http_response_code(403);
+    echo 'Доступ закрыт';
+    exit;
 ?>
