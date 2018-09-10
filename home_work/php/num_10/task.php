6c21e810fbdc54c4213a3d0ad61bf08bc2da89a7
almong <admin@ag-net.ru>
2018-09-10 22:38:57 +0300

    s

--- home_work/php/num_10/task.php
+++ home_work/php/num_10/task.php
@@ -8,30 +8,56 @@
 
     function listTask($id, $db)
     {
-        $sql = "SELECT * FROM task WHERE `id` = '$id'";
+        $sql = "SELECT * FROM task WHERE `user_id` = '$id' ORDER BY `date_added`";
         $preSql = $db->prepare($sql);
         $preSql->execute();
         return $preSql->fetchALL(PDO::FETCH_ASSOC);
     }
 
     function tableTemlate($row)
-    { ?>
+    { print_r($row);
+        ?>
         <tr>
             <td><?php echo $row['id'];?></td>
             <td><?php echo $row['description'];?></td>
-            <td><?php echo $row['date'];?></td>
+            <td><?php echo $row['date_added'];?></td>
             <td><?php echo $row['user_id'];?></td>
             <td><?php echo $row['assigned_user_id'];?></td>
             <td><?php echo $row['is_done'];?></td>
-            <td><a href="<?php deleteTask($row['id']);?>">Удалить</a></td>
+            <td><a href="task.php?del=<?php echo $row['id']?>">Удалить</a></td>
         </tr>
     <?php }
+
+    function addTask($task, $id, $db)
+    {
+        $date = date('Y-m-d h:m:s');
+        echo $date;
+        $sql = "INSERT INTO task(description, date_added, user_id, assigned_user_id, is_done) 
+        VALUE ('$task', '$date', '$id', '$id', '0')";
+        $preSql = $db->prepare($sql);
+        $preSql->execute();
+    }
+
+    function deleteTask($id, $idTask, $db)
+    {
+        $sql = "DELETE FROM task WHERE user_id='$id' AND id='$idTask' LIMIT 1";
+        $preSql = $db->prepare($sql);
+        $preSql->execute();
+    }
+
 
     if (empty($_SESSION['logged_user'])){
             http_response_code(403);
         die;
     } else {
-        
+        if (!empty($_GET)){
+            if ($_GET['add'] == 1){
+                addTask($_GET['task'], $_SESSION['logged_user']['id'], $db);
+            }
+            if (!empty($_GET['del'])){
+                deleteTask($_SESSION['logged_user']['id'], $_GET['del'], $db);
+            }
+        }    
         
 ?>
 
@@ -48,7 +74,7 @@
     <a href="logout.php">Выйти</a>
     <form method="get">
         <input type="text" placeholder="Описание задачи" name="task">
-        <button type="submit">Добавить</button>
+        <button type="submit" name="add" value="1">Добавить</button>
     </form>
     <form method="get">
         <p>Сортировать по: </p>
@@ -57,7 +83,7 @@
             <option value="status">По статусу</option>
             <option value="description">По описанию</option>
         </select>
-        <button type="submit">Сортировать</button>
+        <button type="submit" name="sort" value="1">Сортировать</button>
     </form>
         <table>
             <thead>
@@ -83,4 +109,6 @@
 </body>
 </html>
 
-<?php } ?>
+<?php } 
+    $db = null;
+?>
