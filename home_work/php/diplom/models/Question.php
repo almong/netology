79996e9614ae9e10b2da2d73f39774926ff5c313
almong <admin@ag-net.ru>
2018-10-04 10:37:32 +0300

    admin panel

--- home_work/php/diplom/models/Question.php
+++ home_work/php/diplom/models/Question.php
@@ -2,13 +2,56 @@
 
 namespace app\models;
 
-use yii\db\ActiveRecord;
+use Yii;
 
 /**
- * Class Question
- * @package app\models
+ * This is the model class for table "question".
+ *
+ * @property int $id
+ * @property int $category_id
+ * @property string $question
+ * @property string $answer
+ * @property int $user_id
+ * @property string $date
+ * @property string $status
  */
-class Question extends ActiveRecord
+class Question extends \yii\db\ActiveRecord
 {
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'question';
+    }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['category_id', 'question', 'user_id', 'status'], 'required'],
+            [['category_id', 'user_id'], 'integer'],
+            [['question', 'answer'], 'string'],
+            [['date'], 'safe'],
+            [['status'], 'string', 'max' => 20],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'category_id' => 'Category ID',
+            'question' => 'Question',
+            'answer' => 'Answer',
+            'user_id' => 'User ID',
+            'date' => 'Date',
+            'status' => 'Status',
+        ];
+    }
 }
--- home_work/php/diplom/models/User.php
+++ home_work/php/diplom/models/User.php
@@ -2,12 +2,45 @@
 
 namespace app\models;
 
-use yii\db\ActiveRecord;
+use Yii;
 
 /**
- * Class User
- * @package app\models
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
  */
-class User extends ActiveRecord
+class User extends \yii\db\ActiveRecord
 {
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'email'], 'required'],
+            [['name', 'email'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'email' => 'Email',
+        ];
+    }
 }
